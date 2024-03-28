55e664a8121ff9f3744ce23eef4cfe2c66b1ead5
rubuy-74 <rubemviscard2635@gmail.com>
2024-03-28 16:06:38 +0000

    Fix Parser

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ void testGraphBasicFunctions();
 int main() {
     Parser parser;
 
-    std::list<std::vector<std::string> > cities = parser.readFile("small_dataset/Cities_Madeira.csv");
-    std::list<std::vector<std::string> > reservoirs = parser.readFile("small_dataset/Reservoirs_Madeira.csv");
-    std::list<std::vector<std::string> > stations = parser.readFile("small_dataset/Stations_Madeira.csv");
-    std::list<std::vector<std::string> > pipes = parser.readFile("small_dataset/Pipes_Madeira.csv");
+    std::list<std::vector<std::string> > cities = parser.parseCities("../small_dataset/Cities_Madeira.csv");
+    std::list<std::vector<std::string> > reservoirs = parser.readFile("../small_dataset/Reservoirs_Madeira.csv");
+    std::list<std::vector<std::string> > stations = parser.readFile("../small_dataset/Stations_Madeira.csv");
+    stations.pop_back();
+    std::list<std::vector<std::string> > pipes = parser.readFile("../small_dataset/Pipes_Madeira.csv");
 
     Dataset dataset = Dataset(cities, pipes, stations, reservoirs);
 
